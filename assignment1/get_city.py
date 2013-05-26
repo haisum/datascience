497d6ecab80d25048aae12c97e4668be3db13437
haisum <haisumbhatti@gmail.com>
2013-05-26 11:25:15 +0400

    Adding twitter stream file for fetching usa tweets. Added an Output file for USA tweets Writing a script to traverse tweets and separate them statewise

---
+++ assignment1/get_city.py
@@ -0,0 +1,82 @@
+import json
+import sys
+
+states = {
+    'AK': 'Alaska',
+    'AL': 'Alabama',
+    'AR': 'Arkansas',
+    'AS': 'American Samoa',
+    'AZ': 'Arizona',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DC': 'District of Columbia',
+    'DE': 'Delaware',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    'GU': 'Guam',
+    'HI': 'Hawaii',
+    'IA': 'Iowa',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'MA': 'Massachusetts',
+    'MD': 'Maryland',
+    'ME': 'Maine',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MO': 'Missouri',
+    'MP': 'Northern Mariana Islands',
+    'MS': 'Mississippi',
+    'MT': 'Montana',
+    'NA': 'National',
+    'NC': 'North Carolina',
+    'ND': 'North Dakota',
+    'NE': 'Nebraska',
+    'NH': 'New Hampshire',
+    'NJ': 'New Jersey',
+    'NM': 'New Mexico',
+    'NV': 'Nevada',
+    'NY': 'New York',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PA': 'Pennsylvania',
+    'PR': 'Puerto Rico',
+    'RI': 'Rhode Island',
+    'SC': 'South Carolina',
+    'SD': 'South Dakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VA': 'Virginia',
+    'VI': 'Virgin Islands',
+    'VT': 'Vermont',
+    'WA': 'Washington',
+    'WI': 'Wisconsin',
+    'WV': 'West Virginia',
+    'WY': 'Wyoming'
+}
+
+state_scores = {}
+
+def main():
+	file = open("output_usa.txt")
+	lines = file.readlines()
+
+	for line in lines:
+		tweet = json.loads(line)
+		if "place" in tweet and tweet["place"] != None:
+			place = tweet["place"]["full_name"]
+			for code,name in states.iteritems():
+				if place.find(", " + code) != -1 or place.find(name) != -1:
+					if code in state_scores:
+						state_scores[code] = state_scores[code] + 1
+					else:
+						state_scores[code] = 1
+	print state_scores
+if __name__ == "__main__":
+	main()
